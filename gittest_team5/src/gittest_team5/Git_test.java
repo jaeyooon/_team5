7441c467f5249b3d5ae095603fe9973ae6760779
wanttobodybuilderifbbpro <wogud114@gmail.com>
2022-09-21 16:26:54 +0900

    message with german languages

--- gittest_team5/src/gittest_team5/Git_test.java
+++ gittest_team5/src/gittest_team5/Git_test.java
@@ -5,6 +5,7 @@ public class Git_test {
 	public static void main(String[] args) {
 		System.out.println("git test1");
 		System.out.println("git test2");
+		System.out.println("Das git test3 ist gut!!");
 	}
 
 }
